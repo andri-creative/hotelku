82b7c49017d97dbfbbbd8f7cb6902c34f46359b3
andri-creative <a8461565@gmail.com>
2026-01-28 20:47:41 +0700

    feat: Introduce a new set of UI components, including a responsive sidebar, avatar, and various navigation elements.

---
+++ components/nav-sistem.tsx
@@ -0,0 +1,42 @@
+"use client";
+
+import { type LucideIcon } from "lucide-react";
+
+import {
+  SidebarGroup,
+  SidebarGroupLabel,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+  useSidebar,
+} from "@/components/ui/sidebar";
+
+export function NavSistem({
+  navSistem,
+}: {
+  navSistem: {
+    name: string;
+    url: string;
+    icon: LucideIcon;
+  }[];
+}) {
+  const { isMobile } = useSidebar();
+
+  return (
+    <SidebarGroup className="group-data-[collapsible=icon]:hidden">
+      <SidebarGroupLabel>Sistem</SidebarGroupLabel>
+      <SidebarMenu>
+        {navSistem.map((item) => (
+          <SidebarMenuItem key={item.name}>
+            <SidebarMenuButton asChild>
+              <a href={item.url}>
+                <item.icon />
+                <span>{item.name}</span>
+              </a>
+            </SidebarMenuButton>
+          </SidebarMenuItem>
+        ))}
+      </SidebarMenu>
+    </SidebarGroup>
+  );
+}
